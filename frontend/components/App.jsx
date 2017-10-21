8bf94659056b64dc8b02c4f6dc8da54b0d590510
Isaac <iseessel@oberlin.edu>
2017-10-21 14:02:18 -0400

    created root and app components

---
+++ frontend/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const app = () => {
+
+  return (
+    <div className="main-app">
+      <h1>Poll Everywhere</h1>
+    </div>
+  );
+
+};
+
+export default app;
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { signupAction, logoutAction, loginAction } from './actions/session_actions';
+import Root from './components/root';
 // import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -16,5 +17,5 @@ document.addEventListener('DOMContentLoaded', () => {
   // TESTING END
 //
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Hello World!</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
