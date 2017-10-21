202c904f342927a71b520b1c919a5d1bcd8341fc
Isaac <iseessel@oberlin.edu>
2017-10-21 15:24:39 -0400

    basic header styling

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
-const app = () => {
+const Header = () => (
+  <header>
+    <div className="left-sidebar">
+      <p>img here!</p>
+    </div>
+
+    <div className="right-sidebar">
+      <a href=""><p>Log In</p></a>
+      <a href="" className="signup"><p>Sign Up</p></a>
+    </div>
+  </header>
+);
+
+const App = () => {
 
   return (
     <div className="main-app">
+      <Header />
       <h1>Poll Everywhere</h1>
     </div>
   );
 
 };
 
-export default app;
+export default App;
