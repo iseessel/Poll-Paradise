7a18c400ab8510d83353c64f8a966071e3c94d28
Isaac <iseessel@oberlin.edu>
2017-10-21 12:06:26 -0400

    created user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   validates :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
+  after_initialize :ensure_session_token
+
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     if user && user.is_password?(password)
@@ -27,6 +29,8 @@ class User < ApplicationRecord
     self.save!
     self.session_token
   end
+
+  private:
 
   def ensure_session_token
     self.sesssion_token ||= SecureRandom.urlsafe_base64(16)
