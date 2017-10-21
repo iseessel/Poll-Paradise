5df2d9a27f6add1248159b7e8290698ec148bfb1
Isaac <iseessel@oberlin.edu>
2017-10-21 11:48:18 -0400

    appropriate model methods for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,31 @@ class User < ApplicationRecord
   validates :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
   def password=(password)
-  end 
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.sesssion_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
 end
