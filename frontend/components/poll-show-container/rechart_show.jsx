e154ee1dd222d9ed40766a4c0262bf1ceae308f6
Isaac <iseessel@oberlin.edu>
2017-11-03 10:02:26 -0400

    added percent to bargraphs

--- frontend/components/poll-show-container/rechart_show.jsx
+++ frontend/components/poll-show-container/rechart_show.jsx
@@ -12,7 +12,6 @@ class CustomTicks extends React.Component{
   }
 
   render(){
-    ;
     if(this.props.payload.value.slice(0, 4) === "http"){
       return(
         <g transform={  "translate( " + this.props.x + "," + this.props.y + " )"}>
@@ -47,10 +46,20 @@ class Chart extends React.Component{
     super(props)
   }
 
+  totalNumResponses(){
+
+    let totalNumResponses = 0
+    this.props.answerChoices.forEach((answerChoice) => {
+      totalNumResponses += answerChoice.timesChosen
+    })
+    return totalNumResponses === 0 ? 1 : totalNumResponses
+  }
+
   generateData(){
+    const totalNumResponses = this.totalNumResponses()
     const data = this.props.answerChoices.map((answerChoice) => {
       const datum = { fill: "#4a89c3",
-        amt: answerChoice.timesChosen,
+        amt: answerChoice.timesChosen/totalNumResponses,
         }
       answerChoice.imageUrl !== "/images/original/missing.png" ?
         datum["name"] = answerChoice.imageUrl :
@@ -61,7 +70,14 @@ class Chart extends React.Component{
     return data
   }
 
+  toPercent(num){
+    return `${num * 100}%`
+  }
+
   render(){
+    const ticks = [
+      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1
+      ];
     return(
         <ResponsiveContainer width='90%' height="95%">
           <BarChart
@@ -71,7 +87,8 @@ class Chart extends React.Component{
             layout="vertical"
             margin={{top: 5, right: 30, left: 20, bottom: 5}}
           >
-            <XAxis type="number"/>
+            <XAxis domain={[0,1]} type="number" tickCount={10}
+              ticks={ticks} tickFormatter={this.toPercent}/>
             <YAxis tick={<CustomTicks/>} type="category" dataKey="name" />
             <Bar dataKey="amt" fill="#82ca9d" />
           </BarChart>
